bfbb8ef151c37d57ee68f4b95ecc52720c76c5fb
Hamza Aburaneh <Hamza.aburaneh@carleton.ca>
2025-12-03 10:48:06 -0500

    feat: update google sheets api to use json token

--- src/main/java/ca/gc/tbs/GoogleSheetsAPI.java
+++ src/main/java/ca/gc/tbs/GoogleSheetsAPI.java
@@ -1,6 +1,7 @@
 package ca.gc.tbs;
 
-import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
+import com.google.auth.http.HttpCredentialsAdapter;
+import com.google.auth.oauth2.GoogleCredentials;
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.JsonFactory;
@@ -12,95 +13,82 @@ import com.google.api.services.sheets.v4.model.ValueRange;
 
 import java.io.IOException;
 import java.security.GeneralSecurityException;
-import java.security.KeyStore;
-import java.security.PrivateKey;
 import java.util.Arrays;
 import java.util.Collections;
 
 public class GoogleSheetsAPI {
 
-    static final String spreadsheetId = "1B16qEbfp7SFCfIsZ8fcj7DneCy1WkR0GPh4t9L9NRSg";
-    static final String duplicateCommentsSpreadsheetId = "1cR2mih5sBwl3wUjniwdyVA0xZcqV2Wl9yhghJfMG5oM"; // Template ID to
-    // be replaced
-    static final String range = "A1:A50000";
-    private static final String APPLICATION_NAME = "My Google Sheets Application";
-    private static final JsonFactory JSON_FACTORY = GsonFactory.getDefaultInstance();
-    private static final String SERVICE_ACCOUNT_EMAIL = "cronjob@feedback-cj.iam.gserviceaccount.com";
-    /**
-     * Global instance of the HTTP transport.
-     */
-    private static NetHttpTransport HTTP_TRANSPORT;
+        static final String spreadsheetId = "1B16qEbfp7SFCfIsZ8fcj7DneCy1WkR0GPh4t9L9NRSg";
+        static final String duplicateCommentsSpreadsheetId = "1cR2mih5sBwl3wUjniwdyVA0xZcqV2Wl9yhghJfMG5oM"; // Template
+                                                                                                             // ID to
+        // be replaced
+        static final String range = "A1:A50000";
+        private static final String APPLICATION_NAME = "My Google Sheets Application";
+        private static final JsonFactory JSON_FACTORY = GsonFactory.getDefaultInstance();
 
-    public static void appendURL(String url) throws GeneralSecurityException, IOException {
-        KeyStore keystore = KeyStore.getInstance("PKCS12");
-        keystore.load(GoogleSheetsAPI.class.getClassLoader().getResourceAsStream("service-account.p12"),
-                "notasecret".toCharArray());
-        PrivateKey pk = (PrivateKey) keystore.getKey("privatekey", "notasecret".toCharArray());
+        public static GoogleCredentials getCredentials() throws IOException {
+                String envKey = System.getenv("GOOGLE_SERVICE_ACCOUNT_KEY");
 
-        final NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
+                if (envKey == null || envKey.isEmpty()) {
+                        throw new IOException("GOOGLE_SERVICE_ACCOUNT_KEY environment variable is missing or empty.");
+                }
 
-        GoogleCredential credential = new GoogleCredential.Builder().setTransport(HTTP_TRANSPORT)
-                .setJsonFactory(JSON_FACTORY)
-                .setServiceAccountId(SERVICE_ACCOUNT_EMAIL)
-                .setServiceAccountScopes(Collections.singleton(SheetsScopes.SPREADSHEETS))
-                .setServiceAccountPrivateKey(pk)
-                .build();
-
-        Sheets service = new Sheets.Builder(HTTP_TRANSPORT, JSON_FACTORY, credential)
-                .setApplicationName(APPLICATION_NAME)
-                .build();
-
-        ValueRange appendBody = new ValueRange()
-                .setValues(Arrays.asList(
-                        Arrays.asList(url)));
-        try {
-            AppendValuesResponse appendResult = service.spreadsheets().values()
-                    .append(spreadsheetId, range, appendBody)
-                    .setValueInputOption("USER_ENTERED")
-                    .setInsertDataOption("INSERT_ROWS")
-                    .setIncludeValuesInResponse(true)
-                    .execute();
-        } catch (IOException e) {
-            e.printStackTrace();
+                return GoogleCredentials
+                                .fromStream(new java.io.ByteArrayInputStream(
+                                                envKey.getBytes(java.nio.charset.StandardCharsets.UTF_8)))
+                                .createScoped(Collections.singleton(SheetsScopes.SPREADSHEETS));
         }
-    }
 
-    public static void appendDuplicateComment(String date, String timestamp, String url, String comment)
-            throws GeneralSecurityException, IOException {
-        KeyStore keystore = KeyStore.getInstance("PKCS12");
-        keystore.load(GoogleSheetsAPI.class.getClassLoader().getResourceAsStream("service-account.p12"),
-                "notasecret".toCharArray());
-        PrivateKey pk = (PrivateKey) keystore.getKey("privatekey", "notasecret".toCharArray());
+        public static void appendURL(String url) throws GeneralSecurityException, IOException {
+                final NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
+                GoogleCredentials credential = getCredentials();
 
-        final NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
+                Sheets service = new Sheets.Builder(HTTP_TRANSPORT, JSON_FACTORY,
+                                new HttpCredentialsAdapter(credential))
+                                .setApplicationName(APPLICATION_NAME)
+                                .build();
 
-        GoogleCredential credential = new GoogleCredential.Builder().setTransport(HTTP_TRANSPORT)
-                .setJsonFactory(JSON_FACTORY)
-                .setServiceAccountId(SERVICE_ACCOUNT_EMAIL)
-                .setServiceAccountScopes(Collections.singleton(SheetsScopes.SPREADSHEETS))
-                .setServiceAccountPrivateKey(pk)
-                .build();
-
-        Sheets service = new Sheets.Builder(HTTP_TRANSPORT, JSON_FACTORY, credential)
-                .setApplicationName(APPLICATION_NAME)
-                .build();
-
-        ValueRange appendBody = new ValueRange()
-                .setValues(Arrays.asList(
-                        Arrays.asList(date, timestamp, url, comment)));
-        try {
-            AppendValuesResponse appendResult = service.spreadsheets().values()
-                    .append(duplicateCommentsSpreadsheetId, "A1:D50000", appendBody)
-                    .setValueInputOption("USER_ENTERED")
-                    .setInsertDataOption("INSERT_ROWS")
-                    .setIncludeValuesInResponse(true)
-                    .execute();
-        } catch (IOException e) {
-            e.printStackTrace();
+                ValueRange appendBody = new ValueRange()
+                                .setValues(Arrays.asList(
+                                                Arrays.asList(url)));
+                try {
+                        AppendValuesResponse appendResult = service.spreadsheets().values()
+                                        .append(spreadsheetId, range, appendBody)
+                                        .setValueInputOption("USER_ENTERED")
+                                        .setInsertDataOption("INSERT_ROWS")
+                                        .setIncludeValuesInResponse(true)
+                                        .execute();
+                } catch (IOException e) {
+                        e.printStackTrace();
+                }
         }
-    }
 
-    public static void main(String[] args) throws GeneralSecurityException, IOException {
-        appendURL("test");
-    }
+        public static void appendDuplicateComment(String date, String timestamp, String url, String comment)
+                        throws GeneralSecurityException, IOException {
+                final NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
+                GoogleCredentials credential = getCredentials();
+
+                Sheets service = new Sheets.Builder(HTTP_TRANSPORT, JSON_FACTORY,
+                                new HttpCredentialsAdapter(credential))
+                                .setApplicationName(APPLICATION_NAME)
+                                .build();
+
+                ValueRange appendBody = new ValueRange()
+                                .setValues(Arrays.asList(
+                                                Arrays.asList(date, timestamp, url, comment)));
+                try {
+                        AppendValuesResponse appendResult = service.spreadsheets().values()
+                                        .append(duplicateCommentsSpreadsheetId, "A1:D50000", appendBody)
+                                        .setValueInputOption("USER_ENTERED")
+                                        .setInsertDataOption("INSERT_ROWS")
+                                        .setIncludeValuesInResponse(true)
+                                        .execute();
+                } catch (IOException e) {
+                        e.printStackTrace();
+                }
+        }
+
+        public static void main(String[] args) throws GeneralSecurityException, IOException {
+                appendURL("test");
+        }
 }
